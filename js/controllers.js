b2aaf396e5cfb00a960483255f95d5df23cd3fdd
vikas <v.malik@yahoo.com>
2015-04-15 08:17:09 -0400

    CHAPTER-2 Custom directives added, not used right now. But will be used in next steps for the alerting and validation functionality

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,16 @@
 parking.controller("parkingCtrl", function ($scope) {
-	$scope.appTitle = "[EXP] Parking";
+	$scope.appTitle = "EXP Parking";
+	$scope.showAlert = true;
+	$scope.alertTopic = "Something went wrong!";
+    $scope.alertMessage = "You must inform the plate and the color of the car!";
+	
 	$scope.cars = [];
 	$scope.colors = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red", "Silver"];
+	
+	
+	$scope.closeAlert = function () {
+		$scope.showAlert = false;
+	};
 	
 	$scope.park = function (car) {
 		car.entrance = new Date();
--- js/directives.js
+++ js/directives.js
@@ -1,5 +1,22 @@
-parking.directive("directiveName", function () {
+parking.directive("alert", function () {
 	return {
-		
+		restrict: 'E',
+		scope: {
+			topic: '=topic',
+			description: '=description',
+			close: '&close'
+		},
+		templateUrl: "alert.html",
+		replace: true,
+		transclude: true
 	};
 });
+
+<!--  if the name of the directive's scope property is the same as of the expression, we can keep just the prefix. By convention, the framework will consider
+the name to be the identical to the scope property name.  
+	scope: {
+			topic: '=',
+			description: '=',
+			close: '&'
+	},
+-->
